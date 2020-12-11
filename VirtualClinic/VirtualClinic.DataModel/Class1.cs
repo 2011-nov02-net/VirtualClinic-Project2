8cb87c5daf63e75162391e86e208309efedf9da1
Josiah Liberatore <josiahliberatore@gmail.com>
2020-12-11 14:19:01 -0800

    initial commit for project files

---
+++ VirtualClinic/VirtualClinic.DataModel/Class1.cs
@@ -0,0 +1,8 @@
+﻿using System;
+
+namespace VirtualClinic.DataModel
+{
+    public class Class1
+    {
+    }
+}
---
+++ VirtualClinic/VirtualClinic.Domain/Class1.cs
@@ -0,0 +1,8 @@
+﻿using System;
+
+namespace VirtualClinic.Domain
+{
+    public class Class1
+    {
+    }
+}
