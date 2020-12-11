954c00f1fce72dc4e10a43e3a85ca09b0e1cb264
Josiah Liberatore <josiahliberatore@gmail.com>
2020-12-11 14:36:50 -0800

    models defined and patient model implemented

---
+++ VirtualClinic/VirtualClinic.Domain/Models/Patient.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace VirtualClinic.Domain.Models
+{
+    public class Patient
+    {
+        public string Name { get; set; }
+        public int Id { get; set; }
+        public Doctor PrimaryDoctor { get; set; }
+        public List<PatientReport> PatientReports { get; set; }
+        public List<Prescription> Prescriptions { get; set; }
+        public List<Appointment> Appointments { get; set; }
+        public DateTime DateOfBirth { get; set; }
+        public string SSN { get; set; }
+        public string InsuranceProvider { get; set; }
+
+    }
+}
