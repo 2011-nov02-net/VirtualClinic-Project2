a89108f882b0054c6abddd28454117c7d9084a9b
Noelwiz <noelwiz1@gmail.com>
2020-12-14 09:01:47 -0800

    added regions to code

--- VirtualClinic/VirtualClinic.Domain/Interfaces/IClinicRepository.cs
+++ VirtualClinic/VirtualClinic.Domain/Interfaces/IClinicRepository.cs
@@ -11,16 +11,19 @@ namespace VirtualClinic.Domain.Interfaces
         /// <summary>
         /// Patient related queries
         /// </summary>
+        #region Patients
         public IEnumerable<Patient> GetPatients();
         public Task<IEnumerable<Patient>> GetPatientsAsync();
         public Patient GetPatientByID(int id);
         public Task<Patient> GetPatientByIDAsync(int id);
         public void AddPatient(Patient patient);
         public Task AddPatientAsync(Patient patient);
+        #endregion
 
         /// <summary>
         /// Doctor related queries
         /// </summary>
+        #region Doctors
         public IEnumerable<Doctor> GetDoctors();
         public Task<IEnumerable<Doctor>> GetDoctorsAsync();
         public Doctor GetDoctorByID(int id);
@@ -29,33 +32,40 @@ namespace VirtualClinic.Domain.Interfaces
         public Task<IEnumerable<Patient>> GetDoctorPatientsAsync(int id);
         public void AddDoctor(Doctor doctor);
         public Task AddDoctorAsync(Doctor doctor);
+        #endregion
 
         /// <summary>
         /// Timeslot related queries
         /// </summary>
+        #region TimeSlot
         public void AddTimeslot(Timeslot timeslot);
         public Task AddTimeslotAsync(Timeslot timeslot);
         public IEnumerable<Timeslot> GetPatientTimeslots(int id);
         public Task<IEnumerable<Timeslot>> GetPatientTimeslotsAsync(int id);
         public IEnumerable<Timeslot> GetDoctorTimeslots(int id);
         public Task<IEnumerable<Timeslot>> GetDoctorTimeslotsAsync(int id);
+        #endregion
 
         /// <summary>
         /// Patient report related queries
         /// </summary>
+        #region Reports
         public IEnumerable<PatientReport> GetPatientReports(int id);
         public Task<IEnumerable<Prescription>> GetPatientReportsAsync(int id);
         public PatientReport GetPatientReportByID(int id);
         public Task<PatientReport> GetPatientReportByIDAsync(int id);
         public void AddPatientReport(PatientReport report);
         public Task AddPatientReportAsync(PatientReport report);
+        #endregion
 
         /// <summary>
         /// Prescription related queries
         /// </summary>
+        #region Perscriptions
         public void AddPrescription(Prescription prescription);
         public Task AddPrescriptionAsync(Prescription prescription);
         public IEnumerable<Prescription> GetPatientPrescriptions(int id);
         public Task<IEnumerable<Prescription>> GetPatientPrescriptionsAsync(int id);
+        #endregion
     }
 }
