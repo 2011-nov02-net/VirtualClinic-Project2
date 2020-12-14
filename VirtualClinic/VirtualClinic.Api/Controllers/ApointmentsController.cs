a668afce5239c60cb65c4a640a48e8b81b063e95
Noelwiz <noelwiz1@gmail.com>
2020-12-13 20:51:59 -0800

    Summary of methods

--- VirtualClinic/VirtualClinic.Api/Controllers/ApointmentsController.cs
+++ VirtualClinic/VirtualClinic.Api/Controllers/ApointmentsController.cs
@@ -13,13 +13,25 @@ namespace VirtualClinic.Api.Controllers
     public class ApointmentsController : ControllerBase
     {
         // GET: api/<ApointmentsController>
+        /// <summary>
+        /// Gets a list of all apointments for the user. This list may be empty.
+        /// </summary>
+        /// <param name="after">
+        /// Filter apointments to only be the ones after this date using Linq.
+        /// </param>
+        /// <returns>A list of all apointments.</returns>
         [HttpGet]
-        public IEnumerable<string> Get()
+        public IEnumerable<string> Get([FromQuery] DateTime after)
         {
             return new string[] { "value1", "value2" };
         }
 
         // GET api/<ApointmentsController>/5
+        /// <summary>
+        /// Gets the information about an apointment.   
+        /// </summary>
+        /// <param name="id"> The apointment's ID</param>
+        /// <returns>The apointments information, 404 not found, or 403 unauthorized</returns>
         [HttpGet("{id}")]
         public string Get(int id)
         {
@@ -27,18 +39,37 @@ namespace VirtualClinic.Api.Controllers
         }
 
         // POST api/<ApointmentsController>
+        /// <summary>
+        /// If patient, create an apointment, if dr create an open timeslot?
+        /// </summary>
+        /// <returns>???</returns>
         [HttpPost]
         public void Post([FromBody] string value)
         {
         }
 
         // PUT api/<ApointmentsController>/5
+        /// <summary>
+        /// updates an apointments details
+        /// </summary>
+        /// <param name="id">The ID of the apointment</param>
+        /// <param name="value">The changes to be made</param>
+        /// <returns>
+        /// 404 not found, 403 unauthorized or something to do with succsess
+        /// </returns>
         [HttpPut("{id}")]
         public void Put(int id, [FromBody] string value)
         {
         }
 
         // DELETE api/<ApointmentsController>/5
+        /// <summary>
+        /// Cancels an apointment freeing the timeslot.
+        /// </summary>
+        /// <param name="id">The id of the apointment</param>
+        /// <returns>
+        /// OK, 404 not found, or 403 not authroized
+        /// </returns>
         [HttpDelete("{id}")]
         public void Delete(int id)
         {
