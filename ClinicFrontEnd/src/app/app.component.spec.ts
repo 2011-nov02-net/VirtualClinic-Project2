724444ca619eed2c71badb97bdbe0bf1d39123ba
dppet <dppet@DANIELOMEN>
2020-12-17 15:51:52 -0800

    added bootstrap

--- ClinicFrontEnd/src/app/app.component.spec.ts
+++ ClinicFrontEnd/src/app/app.component.spec.ts
@@ -16,16 +16,16 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have as title 'VirtualClinicFrontEnd'`, () => {
+  it(`should have as title 'ClinicFrontEnd'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('VirtualClinicFrontEnd');
+    expect(app.title).toEqual('ClinicFrontEnd');
   });
 
   it('should render title', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement;
-    expect(compiled.querySelector('.content span').textContent).toContain('VirtualClinicFrontEnd app is running!');
+    expect(compiled.querySelector('.content span').textContent).toContain('ClinicFrontEnd app is running!');
   });
 });
