c086bcbdb7b3cadf656fc4c2e5195de6b4bb0573
dppet <dppet@DANIELOMEN>
2020-12-19 21:32:37 -0800

    auth set up

--- VirtualClinicFrontEnd/src/app/app-routing.module.ts
+++ VirtualClinicFrontEnd/src/app/app-routing.module.ts
@@ -4,13 +4,15 @@ import { RouterModule, Routes } from '@angular/router';
 import { DoctorsComponent } from '../app/doctors/doctors.component';
 import { PatientDetailComponent} from '../app/patient-detail/patient-detail.component';
 import { LogInComponent } from './log-in/log-in.component';
+import { OktaCallbackComponent } from '@okta/okta-angular';
 
 
 
 const routes: Routes = [
   { path: 'Doctors/:id', component: DoctorsComponent },
-  { path:  'Patients/:id', component: PatientDetailComponent}
-  { path: 'login', component: LogInComponent }
+  { path:  'Patients/:id', component: PatientDetailComponent},
+  { path: 'login', component: LogInComponent },
+  { path: 'login/callback', component: OktaCallbackComponent }
 ];
 
 @NgModule({
