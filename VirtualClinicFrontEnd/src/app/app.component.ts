09fc42904b7eea15d2943a7f9ca86e89cc664520
dppet <dppet@DANIELOMEN>
2020-12-19 20:42:38 -0800

    top nav exists

--- VirtualClinicFrontEnd/src/app/app.component.ts
+++ VirtualClinicFrontEnd/src/app/app.component.ts
@@ -1,10 +1,27 @@
 import { Component } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
+  
 })
 export class AppComponent {
   title = 'VirtualClinicFrontEnd';
+
+  links = [
+    /*{titel: link display text, }
+     * fragment: url fragment. shows up as "#fragment" appended at the end,
+     * page: the page to route to}
+     */
+    { title: 'One', fragment: 'one', page:"." },
+    { title: 'Two', fragment: 'two', page:"." },
+    { title: 'Log In', fragment: '', page:"login" }
+  ];
+
+  //https://ng-bootstrap.github.io/#/components/nav/overview#routing
+  //gives the component a ref to the active route for the nav
+  constructor(public route: ActivatedRoute) {}
 }
