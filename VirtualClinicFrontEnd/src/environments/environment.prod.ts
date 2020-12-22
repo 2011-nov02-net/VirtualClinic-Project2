4fab2ddf4374068125c9495326bdb57ec3c3f0a4
dppet <dppet@DANIELOMEN>
2020-12-22 12:08:53 -0800

    removed unused env var

--- VirtualClinicFrontEnd/src/environments/environment.prod.ts
+++ VirtualClinicFrontEnd/src/environments/environment.prod.ts
@@ -1,5 +1,4 @@
 export const environment = {
   production: true,
   urlBase: "https://virtual-clinic-backend.azurewebsites.net/Api/", 
-  frontEndUrl: "https://virtual-clinic.azurewebsites.net"
 };
