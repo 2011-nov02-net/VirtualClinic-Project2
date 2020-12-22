1c5f0d2ed1a4e9392eeb9391ce07d315893b09f7
Josiah Liberatore <josiahliberatore@gmail.com>
2020-12-21 20:29:31 -0800

    database filled with test data

--- SQL/clinicDML.sql
+++ SQL/clinicDML.sql
@@ -8,55 +8,56 @@ INSERT INTO Clinic.Doctors (Id,Name, Title) VALUES
      (3,'Perry Cox', 'Oncologist'),
      (4,'Dana Scully', 'Radiologist');
 
-INSERT INTO Clinic.Patients (Id, Name, DoctorId, DOB, SSN, Insurance) VALUES 
-      (1,'Walter White',1,'2015-12-17','626-70-4664', 'Aetna'),
-      (2,'Tyrion Lannister',2,'1997-06-21','004-36-9199', 'BlueCross BlueShield'),
-      (3,'Sherlock Holmes',3,'1951-08-30','039-60-1334', 'National General'),
-      (4,'Chandler Bing',4,'1994-10-10','516-49-5594', 'Aetna'),
-      (5,'Michael Scott',1,'2009-09-21','681-24-2024', 'Cigna'),
-      (6,'Dean Winchester',2,'1979-03-19','042-60-4547', 'Kaiser Permanente');
+INSERT INTO Clinic.Patients (Name, DoctorId, DOB, SSN, Insurance) VALUES 
+      ('Walter White',1,'2015-12-17','626-70-4664', 'Aetna'),
+      ('Tyrion Lannister',2,'1997-06-21','004-36-9199', 'BlueCross BlueShield'),
+      ('Sherlock Holmes',3,'1951-08-30','039-60-1334', 'National General'),
+      ('Chandler Bing',4,'1994-10-10','516-49-5594', 'Aetna'),
+      ('Michael Scott',1,'2009-09-21','681-24-2024', 'Cigna'),
+      ('Dean Winchester',2,'1979-03-19','042-60-4547', 'Kaiser Permanente');
 
-INSERT INTO Clinic.Vitals ( Id, Systolic, Diastolic, HeartRate,Temperature, Pain) VALUES 
-       (1,120, 80, 95,97.2, 2),
-       (2,143, 91, 93,96.3, 1),
-       (3,123, 84, 88,97.2, 1),
-       (4,111, 75, 115,99.0, 2),
-       (5,170, 95, 75,96.8, 3),
-       (6,123, 65, 83,98.1, 4),
-       (7,134, 64, 149,96.7, 8),
-       (8,138, 90, 112,97.5, 10);
-INSERT INTO Clinic.PatientReports ( Id,PatientId, Information, ReportTime, VitalsId) VALUES
-       (1,1, 'Lorem Ipsum1', GetDate(), 1),
-       (2,2, 'Lorem Ipsum2', GetDate(), 2),
-       (3,3, 'Lorem Ipsum3', GetDate(), 3),
-       (4,4, 'Lorem Ipsum4', GetDate(), 4),
-       (5,5, 'Lorem Ipsum5', GetDate(), 5),
-       (6,6, 'Lorem Ipsum1', GetDate(), 6),
-       (7,1, 'Lorem Ipsum1', GetDate(), 7),
-       (8,2, 'Lorem Ipsum6', GetDate(), 8);
+INSERT INTO Clinic.Vitals (Systolic, Diastolic, HeartRate,Temperature, Pain) VALUES 
+       (120, 80, 95,97.2, 2),
+       (143, 91, 93,96.3, 1),
+       (123, 84, 88,97.2, 7),
+       (111, 72, 115,99.0, 2),
+       (170, 95, 75,96.8, 3),
+       (123, 65, 83,98.1, 4);
 
-INSERT INTO Clinic.Prescriptions ( Id,PatientId, DoctorId, Drug, Information, [Date]) VALUES
-       ( 1,1, 1,'Levothyroxine','Lorem Ipsum1' , GETDATE()),
-       ( 2,2, 2,'Lisinopril','Lorem Ipsum2' , GETDATE()),
-       ( 3,3, 3,'Omeprazole','Lorem Ipsum3' , GETDATE()),
-       ( 4,1, 1,'Albuterol','Lorem Ipsum4' , GETDATE()),
-       (5, 2, 2,'Simvastatin','Lorem Ipsum5' , GETDATE());
+INSERT INTO Clinic.PatientReports (PatientId, Information, ReportTime, VitalsId) VALUES
+       (1, 'Lorem Ipsum1', GetDate(), 5),
+       (2, 'Lorem Ipsum2', GetDate(), 6),
+       (3, 'Lorem Ipsum3', GetDate(), 7),
+       (4, 'Lorem Ipsum4', GetDate(), 8),
+       (5, 'Lorem Ipsum5', GetDate(), 9),
+       (6, 'Lorem Ipsum1', GetDate(), 10),
+       (1, 'Lorem Ipsum1', GetDate(), NULL),
+       (2, 'Lorem Ipsum6', GetDate(), NULL);
 
-INSERT INTO Clinic.Appointments (Id, Notes, VitalsId, PatientId)   VALUES
-       (1,'Lorem Ipsum1',1,1),   
-       (2,'Lorem Ipsum2',2,2), 
-       (3,'Lorem Ipsum3',3,3), 
-       (4,'Lorem Ipsum4',4,4), 
-       (5,'Lorem Ipsum5',5,5), 
-       (6,'Lorem Ipsum6',6,6);
+INSERT INTO Clinic.Prescriptions (PatientId, DoctorId, Drug, Information, [Date]) VALUES
+       (1, 1,'Levothyroxine','Lorem Ipsum1' , GETDATE()),
+       (2, 2,'Lisinopril','Lorem Ipsum2' , GETDATE()),
+       (3, 3,'Omeprazole','Lorem Ipsum3' , GETDATE()),
+       (1, 1,'Albuterol','Lorem Ipsum4' , GETDATE()),
+       (2, 2,'Simvastatin','Lorem Ipsum5' , GETDATE());
 
-INSERT INTO Clinic.Timeslots ( Id,DoctorId, AppointmentId)  VALUES
-       (1,1,1),
-       (2,2,2),
-       (3,3,3),
-       (4,4,4),
-       (5,1,5),
-       (6,2,6);
+INSERT INTO Clinic.Appointments (Notes, DoctorId, PatientId, VitalsId, [Start], [End])   VALUES
+       ('Lorem Ipsum1',1, 1, 5, '2020-12-20 12:30:00', '2020-12-20 13:00:00'),
+       ('Lorem Ipsum2',2, 2, 6, '2020-12-20 9:30:00', '2020-12-20 11:00:00'), 
+       ('Lorem Ipsum3',3, 3, 7, '2020-12-20 12:30:00', '2020-12-20 13:00:00'), 
+       ('Lorem Ipsum4',4, 4, 8, '2020-12-20 12:00:00', '2020-12-20 13:00:00'), 
+       ('Lorem Ipsum5',1, 1, 9, '2020-12-26 12:30:00', '2020-12-26 13:00:00'), 
+       ('Lorem Ipsum6',2, 2, 10, '2020-12-30 12:30:00', '2020-12-30 13:00:00');
+
+INSERT INTO Clinic.Timeslots (DoctorId, AppointmentId, [Start], [End])  VALUES
+       (1,8, '2020-12-20 12:30:00', '2020-12-20 13:00:00'),
+       (2,9, '2020-12-20 9:30:00', '2020-12-20 11:00:00'),
+       (3,10, '2020-12-20 12:30:00', '2020-12-20 13:00:00'),
+       (4,11, '2020-12-20 12:00:00', '2020-12-20 13:00:00'),
+       (1,12, '2020-12-26 12:30:00', '2020-12-26 13:00:00'),
+       (2,13, '2020-12-30 12:30:00', '2020-12-30 13:00:00'),
+	   (1, NULL, '2020-12-25 1:30:00', '2020-12-25 2:30:00'),
+	   (1, NULL, '2020-12-26 15:00:00', '2020-12-26 15:30:00');
 
 select * from Clinic.Doctors
 select * from Clinic.Appointments
@@ -65,3 +66,5 @@ select * from Clinic.Patients
 select * from Clinic.Timeslots
 select * from Clinic.Vitals
 select * from Clinic.Prescriptions
+
+delete from Clinic.Appointments
