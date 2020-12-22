c4703587ea583a0d04a0cb8891217fcb3fcf4837
Oladipo Osinubi <diposinubi@gmail.com>
2020-12-21 22:42:21 -0500

    patient detail html

--- VirtualClinicFrontEnd/src/app/app-routing.module.ts
+++ VirtualClinicFrontEnd/src/app/app-routing.module.ts
@@ -2,10 +2,14 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { LogInComponent } from './log-in/log-in.component';
 import { OktaCallbackComponent } from '@okta/okta-angular';
+import { DoctorsComponent} from '../app/doctors/doctors.component';
+import { PatientsComponent } from '../app/patients/patients.component';
 
 const routes: Routes = [
   { path: 'login', component: LogInComponent },
-  { path: 'login/callback', component: OktaCallbackComponent }
+  { path: 'login/callback', component: OktaCallbackComponent },
+  { path: 'Doctors/1', component: DoctorsComponent },
+  { path:  'Patients/:id', component: PatientsComponent}
 ];
 
 @NgModule({
--- VirtualClinicFrontEnd/src/app/patients/patients.component.ts
+++ VirtualClinicFrontEnd/src/app/patients/patients.component.ts
@@ -1,5 +1,10 @@
 import { Component, OnInit } from '@angular/core';
 import { Patient } from '../models/patient';
+import { Prescription } from '../models/prescription';
+import { PatientReports } from '../models/patientreport';
+import { ActivatedRoute } from '@angular/router';
+import { Location } from '@angular/common';
+import { PatientsService } from '../services/patients.service';
 
 @Component({
   selector: 'app-patients',
@@ -7,10 +12,33 @@ import { Patient } from '../models/patient';
   styleUrls: ['./patients.component.scss']
 })
 export class PatientsComponent implements OnInit {
-
-  constructor() { }
+   patient: Patient | undefined;
+   selectedPrescrition: Prescription | undefined;
+   selectedReport: PatientReports | undefined;
+  constructor(
+    private route: ActivatedRoute,
+    private location: Location,
+    private patientService: PatientsService
+  ) { }
 
   ngOnInit(): void {
   }
+  getPatientByID(id: number): void {
+    this.patientService.getPatientByID(id)
+      .then(patient => {
+        this.patient = patient;
+      })
+  }
 
+  onSelect(prescription: Prescription): void {
+    this.selectedPrescrition = prescription;
+  };
+
+  // onSelect(report: PatientReports): void {
+  //   this.selectedReport = report;
+  // };
+
+  goBack(): void {
+    this.location.back();
+  }
 }
